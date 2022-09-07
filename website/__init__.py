7030e623f5d86facdda0ef86e9d3a13cd780ac63
alanlai1234 <lai.abc8660@gmail.com>
2022-09-07 14:39:25 +0800

    update style

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     # app.config['SECRET_KEY'] = 'momoeqw i0305 woiuqwhogfw'
-    app.config['TEMPLATES_AUTO_RELOAD'] = True      
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.jinja_env.auto_reload = True
     
     from .views import views
