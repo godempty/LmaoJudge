821ad6c2f0c1d60d27d4d7736bb1c59b91ec9279
朱修毅 <qmore0305@gmail.com>
2022-09-11 12:45:17 +0800

    fix pymongo

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from http import client
 from website import create_app
-import pymongo
-
-client = pymongo.MongoClient('localhost', 27017)
-db = client.LmaoJudge
 
 app = create_app()
 
--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,4 @@
 from flask import Flask
-import pymongo
-
-client = pymongo.MongoClient('localhost', 27017)
-db = client.LmaoJudge
 
 
 def create_app():
