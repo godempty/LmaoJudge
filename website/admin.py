4a2ac4348bdffc9899bcb8edb468a88a03b5b4eb
朱修毅 <qmore0305@gmail.com>
2022-09-21 18:33:30 +0800

    addannouncement

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,4 @@
+from datetime import date, datetime
 from flask import Blueprint, render_template, request, redirect
 from .db import db
 
@@ -6,3 +7,19 @@ admin = Blueprint('admin', __name__)
 @admin.route('/')
 def home():
     return render_template("admin.html")
+
+@admin.route('/addannounce', methods=['POST','GET'])
+def addannounce():
+    if request.method == 'POST':
+        today =datetime.utcnow()
+        announcement = {
+            'title': request.form.get('title'),
+            'context': request.form.get('context'),
+            'time': today,
+            'author': request.form.get('author')
+        }
+        result = db['announcements'].insert_one(announcement)
+    return render_template("addannounce.html")
+@admin.route('/addproblems')
+def addproblems():
+    return render_template("addproblems.html")
