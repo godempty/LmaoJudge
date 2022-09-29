c9c35a718f5e2db9e26944b7276ed00d59ba3843
alanlai1234 <lai.abc8660@gmail.com>
2022-09-29 22:01:14 +0800

    hand merge judging system

---
+++ website/judging/__init__.py
@@ -0,0 +1,55 @@
+from ..db import db
+import subprocess, os, time
+
+def default_validator(exe, test, ans, lang, time, file, memory):
+    execute = object()
+    cmd = list()
+    # assign run command for specific language
+    if(lang == 'c++'):
+        cmd = [file]
+    
+    try:
+        execute = subprocess.Popen(cmd, text=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        ret = execute.communicate(input=test, timeout=time)
+
+        if(execute.returncode != 0):
+            return 0
+
+        if(ret[0].strip() == ans):
+            return 3
+        else:
+            return 2
+
+    except subprocess.TimeoutExpired:
+        execute.kill()
+        return 1
+
+def judgement(pid, code, lang, subid):
+    data = db['problem_test_data'].find_one({'_id': int(pid)})
+    
+    file = os.path.abspath(os.path.dirname(__file__) + '/exes/'+str(subid))
+    #compile
+    if(lang == 'c++'):
+        compil = subprocess.run(['g++', '-o', file, '-xc++', '-'], text=True, input=code, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if(compil.returncode == 1):
+            db['submission_tmp'].update_one({'_id': subid}, {'$set': {'done': 1, 'finalverdict': 'compile error'}})
+            return
+
+    save = [0, 0, 0, 0] #re tle wa ac
+    name = ['RE', 'TLE', 'WA', 'AC']
+    #start testing
+    for tasks in data['subtasks']:
+        for i in range(tasks['total']):
+            save[default_validator(subid, tasks['test'][i], tasks['ans'][i], lang, data['timelimit'], file, data['memlimit'])] = 1
+
+    # verdict
+    verdict = str()
+    for a in range(4):
+        if(save[a] == 1):
+            verdict = name[a]
+            break
+    db['submission_tmp'].update_one({'_id': subid}, {'$set': {'done': 1, 'finalverdict': verdict}})
+
+    #delete executable
+    if(lang == 'c++'):
+        os.remove(file)
--- website/views.py
+++ website/views.py
@@ -1,44 +1,67 @@
-from flask import Blueprint, render_template, request, redirect, session
+from flask import Blueprint, render_template, request, redirect, session, url_for, jsonify
 from .db import db
+from .judging import judgement
 from bson.objectid import ObjectId
 from bson import json_util
-import json
+import json, threading
 views = Blueprint('views', __name__)
+
 # @ is the way to create(define) blueprint
 @views.route('/')
 def home():
     announce = db['announcements'].find()
     return render_template("home.html", announce = announce)
+
 @views.route('/problems')
 def problems():
     return render_template("problems.html")
+
 @views.route('/problems/<id>')
 def problem_page(id):
     return render_template("problem_page.html", pid=id)
+
 @views.route('/contests')
 def contests():
     return render_template("contests.html")
+
 @views.route('/submissions')
 def submissions():
-
     return render_template("submissions.html")
-@views.route('/submit/<id>')
-def submit(id):
 
-    return render_template("submit.html", id=id)
-@views.route('/submit', methods = ['POST', 'GET'])
-def getcode():
+@views.route('/submit/<id>', methods = ['POST', 'GET'])
+def submit(id):
     if(request.method == 'POST'):
         code = request.form['code']
-        print(code)
-        return render_template("submissions.html")
-    else:
-        return 'error'
+        lang = request.form['lang']
+
+        # create submission
+        subid = db['submission_count'].find_one()['num']+1
+        db['submission_count'].update_one({}, {'$set': {'num': subid}})
+        db['submission_tmp'].insert_one({'_id': subid, 'done': 0, 'finalverdict': ''})
+
+        #judge in another thread
+        td = threading.Thread(target = judgement, args = [id, code, lang, subid])
+        td.start()
+        return redirect(url_for('views.single_submission', id=subid))
+
+    return render_template("submit.html", id=id)
+
 @views.route('/announce/<id>')
 def getannounce(id):
     ann = db['announcements'].find_one({"_id": ObjectId(id)})
     session['ann'] = json.loads(json_util.dumps(ann))
     return redirect('/announce')
+
 @views.route('/announce')
 def showannounce():
     return render_template('announcement.html')
+
+@views.route('/submissions/<id>', methods = ['POST', 'GET'])
+def single_submission(id):
+    return render_template("single_submission.html", id=id)
+
+# to respond to frontend ajax
+@views.route('/submissions/<id>/get_data')
+def get_submission_data(id):
+    get = db['submission_tmp'].find_one({'_id': int(id)})
+    return jsonify(get)
