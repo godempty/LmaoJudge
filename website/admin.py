a6b45891a1a4feb6b2a6e45ee2ccd9463583e9b8
godempty <qmore0305@gmail.com>
2024-04-03 19:36:05 +0800

    fix vulnerability of delete announcement

--- website/admin.py
+++ website/admin.py
@@ -10,40 +10,52 @@ admin = Blueprint('admin', __name__)
 
 @admin.route('/')
 def home():
-    return render_template("admin/control_list.html")
+    if(session['user']):
+        return render_template("admin/control_list.html")
+    else:
+        return redirect('/')
 
 @admin.route('/addannounce', methods=['POST','GET'])
 def addannounce():
-    announce = db['announcements'].find()
-    if request.method == 'POST':
-        today =datetime.now()
-        announcement = {
-            'title': request.form.get('title'),
-            'context': request.form.get('context'),
-            'time': f"{today.year}/{today.month}/{today.day}",
-            'author': session['user']['name'],
-        }
+    if(session['user']):
+        announce = db['announcements'].find()
+        if request.method == 'POST':
+            today =datetime.now()
+            announcement = {
+                'title': request.form.get('title'),
+                'context': request.form.get('context'),
+                'time': f"{today.year}/{today.month}/{today.day}",
+                'author': session['user']['name'],
+            }
 
-        db['announcements'].insert_one(announcement)
-    return render_template("admin/announcescontrol.html", announce = announce)
+            db['announcements'].insert_one(announcement)
+        return render_template("admin/announcescontrol.html", announce = announce)
+    else:
+        return redirect('/')
 
 @admin.route('/addproblems/', methods=['POST','GET'])
 def addproblems():
-    type = str(request.args.get('sol'))
-    if request.method == 'POST':
-        problem = new_problem(request)
-        zip_data = request.files['test_data']
-        zip_obj_data = zip_data.stream._file
-        datas = ZipFile(zip_obj_data)
-        # filenames = [f_name for f_name in filenames if f_name.endswith('.in')] 
-        os.makedirs(f"test_data/{problem['pid']}")
-        datas.extractall(path=f"test_data/{problem['pid']}")
-        #print(test_data.read())
-        db['problems'].insert_one(problem)
+    if(session['user']):
+        type = str(request.args.get('sol'))
+        if request.method == 'POST':
+            problem = new_problem(request)
+            zip_data = request.files['test_data']
+            zip_obj_data = zip_data.stream._file
+            datas = ZipFile(zip_obj_data)
+            # filenames = [f_name for f_name in filenames if f_name.endswith('.in')] 
+            os.makedirs(f"test_data/{problem['pid']}")
+            datas.extractall(path=f"test_data/{problem['pid']}")
+            #print(test_data.read())
+            db['problems'].insert_one(problem)
 
-    return render_template("admin/problemscontrol.html", type = type)
+        return render_template("admin/problemscontrol.html", type = type)
+    else:
+        return redirect('/')
 
 @admin.route('/announcedelete/<id1>')
 def delete(id1):
-    db['announcements'].delete_one({"_id": ObjectId(id1)})
-    return redirect('/admin/addannounce')
+    if(not session['user'] or not session['user']['admin']):
+        return redirect('/')
+    else:
+        db['announcements'].delete_one({"_id": ObjectId(id1)})
+        return redirect('/admin/addannounce')
